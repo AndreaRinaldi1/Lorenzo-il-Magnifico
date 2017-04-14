f4916c322f05a13278a04171fbc2057546c1e115
AndreaRinaldi1 <andrea5.rinaldi@mail.polimi.it>
2017-04-14 18:57:16 +0200

    Trying to push in a branch

---
+++ src/main/java/it/polimi/ingsw/GC_28/Last.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.GC_28;
+
+
+public class App 
+{
+	    public static void main( String[] args )
+		        {
+				        System.out.println( "This is the last attempt" );
+					    }
+}
