08d709977f8596da41e42b2150fac77ad665f2ec
Roberto Turi <10489802@polimi.it>
2017-04-29 20:34:12 +0200

    Test
    Interfaccia Github e Copia Repository

--- src/main/java/it/polimi/ingsw/GC_28/App.java
+++ src/main/java/it/polimi/ingsw/GC_28/App.java
@@ -6,5 +6,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "This is the group GC_28" );
+        System.out.println("I'm panicking!");
     }
 }
