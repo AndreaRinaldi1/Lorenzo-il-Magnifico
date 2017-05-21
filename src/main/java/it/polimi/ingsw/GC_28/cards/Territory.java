46df238bf543445792783ee043eb2161ab7aad9b
s-Nick <nicolo.scipione@mail.polimi.it>
2017-05-21 18:59:28 +0200

    Added specific case for the setImmediate effect method of Territory class

--- src/main/java/it/polimi/ingsw/GC_28/cards/Territory.java
+++ src/main/java/it/polimi/ingsw/GC_28/cards/Territory.java
@@ -4,6 +4,10 @@ import it.polimi.ingsw.GC_28.effects.*;
 import java.awt.Color;
 import java.util.ArrayList;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 public class Territory extends Card{
 	private ArrayList<Effect> immediateEffect = new ArrayList<Effect>();
 	private HarvestEffect permanentEffect;
@@ -17,8 +21,28 @@ public class Territory extends Card{
 		return immediateEffect;
 	}
 
-	public void setImmediateEffect(ArrayList<Effect> immediateEffect) {
-		this.immediateEffect = immediateEffect;
+	public void setImmediateEffect(Gson gson, JsonObject j) {
+		JsonArray array = j.get("immediateEffect").getAsJsonArray();
+		for(int i = 0; i<array.size(); i++){
+			JsonObject e = array.get(i).getAsJsonObject();
+			Effect v;
+			switch(e.get("type").getAsString()){
+			case("RESOURCEEFFECT"):
+        		System.out.println("cambio il tipo di immediate effect #" + i + " in ResourceEffect");
+        		v = immediateEffect.get(i);
+        		v = gson.fromJson(e.toString(), ResourceEffect.class);
+        		immediateEffect.set(i, v);
+        		System.out.println(immediateEffect.get(i).getClass());
+        		break;
+			case("PRIVILEGESEFFECT"):
+        		System.out.println("cambio il tipo di immediate effect #" + i + " in PrivilegesEffect");
+        		v = immediateEffect.get(i);
+        		v = gson.fromJson(e.toString(), PrivilegesEffect.class);
+        		immediateEffect.set(i, v);
+        		System.out.println(immediateEffect.get(i).getClass());
+        		break;
+			}
+		}
 	}
 
 	public HarvestEffect getPermanentEffect() {
