21a75e74c70b97cbe0a1673acf0e2aa1a94377b7
s-Nick <nicolo.scipione@mail.polimi.it>
2017-05-25 22:26:04 +0200

    Fix minor vulnerabilities

--- src/main/java/it/polimi/ingsw/GC_28/cards/CardReader.java
+++ src/main/java/it/polimi/ingsw/GC_28/cards/CardReader.java
@@ -4,16 +4,10 @@ package it.polimi.ingsw.GC_28.cards;
 import com.google.gson.*;
 import com.google.gson.stream.JsonReader;
 
-import it.polimi.ingsw.GC_28.boards.GameBoard;
-import it.polimi.ingsw.GC_28.components.DiceColor;
-import it.polimi.ingsw.GC_28.components.FamilyMember;
-import it.polimi.ingsw.GC_28.effects.Effect;
+
 import it.polimi.ingsw.GC_28.effects.NoCellBonusEffect;
 import it.polimi.ingsw.GC_28.effects.PrivilegesEffect;
 import it.polimi.ingsw.GC_28.effects.ResourceEffect;
-import it.polimi.ingsw.GC_28.model.Game;
-import it.polimi.ingsw.GC_28.model.Player;
-import it.polimi.ingsw.GC_28.model.PlayerColor;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -30,10 +24,6 @@ public class CardReader{
     }
 
     public Deck startRead() throws FileNotFoundException{
-        //JsonReader reader = new JsonReader(new FileReader("cards2.json"));
-        //JsonReader reader2 = new JsonReader(new FileReader("cards2.json"));
-        
-
         Deck deck = new Deck();
         try{
         	JsonReader reader = new JsonReader(new FileReader("cards.json"));
@@ -56,8 +46,6 @@ public class CardReader{
             	JsonObject j = arrayCharacter.get(i).getAsJsonObject();
             	deck.getCharacters().get(i).setPermanentEffect(gson, j);
             	deck.getCharacters().get(i).setImmediateEffect(gson, j);
-            	//Character character = new Character(j);
-            	//deck.getCharacters().add(character);
         	}
         	
         	for(int i = 0; i < arrayTerritories.size(); i++){
@@ -65,8 +53,6 @@ public class CardReader{
             	deck.getTerritories().get(i).setImmediateEffect(gson, j);
         	}
         	
-        	//deck.getTerritories().get(0).getPermanentEffect().apply(new FamilyMember(new Player(PlayerColor.BLUE), true, DiceColor.BLACK), new GameBoard(), new Game());
-        	//System.out.println(e.getClass());
         	deck.toString();
         	if(deck.getVentures().get(0).getPermanentEffect() instanceof ResourceEffect){
         		System.out.println("Andrea");
@@ -79,14 +65,6 @@ public class CardReader{
         		NoCellBonusEffect e = (NoCellBonusEffect) deck.getCharacters().get(0).getPermanentEffect();
         		System.out.println(e.isPresence());
         	}
-        	//System.out.println(deck.getCharacters().get(0).getPermanentEffect().getClass());
-        	//System.out.println(deck.getCharacters().get(0).getImmediateEffect().get(0).getClass());
-        	//System.out.println(deck.getCharacters().get(0).getImmediateEffect().get(1).getClass());
-
-        	//System.out.println(d.getBuildings().get(0));
-        	//d.getDeck().get(CardType.VENTURE).get(0).
-        //	System.out.println(d.toString());   
-        	//System.out.println(d.getVentures().get(0).getAlternativeCostPresence());
 	        reader.close();
 	        reader2.close();
     	}
