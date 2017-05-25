e4b211996f4a9d6c2e776e98c1d1436bbd977a57
Roberto Turi <10489802@polimi.it>
2017-05-25 11:25:51 +0200

    added some test
    Fixed toString method of Card.java

---
+++ src/test/java/it/polimi/ingsw/GC_28/cardsTest/TerritoryTest.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.GC_28.cardsTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class TerritoryTest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testGetImmediateEffect() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPermanentEffect() {
+		fail("Not yet implemented");
+	}
+
+}
