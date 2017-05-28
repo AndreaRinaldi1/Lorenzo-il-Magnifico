ba92da724992f8d1fe6fdd53779dbf82ced86285
Roberto Turi <10489802@polimi.it>
2017-05-28 22:56:49 +0200

    added ResourceTest

--- src/test/java/it/polimi/ingsw/GC_28/componentsTest/ResourceTest.java
+++ src/test/java/it/polimi/ingsw/GC_28/componentsTest/ResourceTest.java
@@ -12,15 +12,23 @@ import org.junit.Test;
 import it.polimi.ingsw.GC_28.components.*;
 
 public class ResourceTest {
-	private EnumMap<ResourceType, Integer> resource;
-	private Resource bonus;
+	private EnumMap<ResourceType, Integer> resource, resource1, resource2;
+	private Resource bonus, bonus1, bonus2, bonus3;
 	
 	@Before
 	public void resource(){
+		 
 		resource = new EnumMap<ResourceType, Integer>(ResourceType.class);
 		resource.put(ResourceType.STONE, 2);
 		bonus = Resource.of(resource);
-		
+		bonus3 = Resource.of(resource);
+		resource1 = new EnumMap<ResourceType, Integer>(ResourceType.class);
+		resource1.put(ResourceType.FAITHPOINT, 6);
+		bonus1 = Resource.of(resource1);
+		resource2 = new EnumMap<ResourceType, Integer>(ResourceType.class);
+		bonus2 = Resource.of(resource2);
+		bonus2.modifyResource(bonus1, true);
+	
 	}
 	
 	
@@ -29,19 +37,24 @@ public class ResourceTest {
 	}
 
 	@Test
-	public void testOf() {
-		assertEquals(this.bonus, Resource.of(resource));
-		//fail("Not yet implemented");
+	public void testOf(){
+		assertEquals(true, this.bonus.equals(Resource.of(resource)));
 	}
-
+	
 	@Test
 	public void testGetResource() {
+		assertEquals(this.resource, this.bonus.getResource());
 		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testModifyResource() {
+		assertEquals(true, bonus1.equals(bonus2));
 		//fail("Not yet implemented");
 	}
-
+	
+	@Test 
+	public void testEquals(){
+		assertEquals(true, this.bonus.equals(bonus3));
+	}
 }
