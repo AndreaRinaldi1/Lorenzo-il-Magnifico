aa78fd5851606d19f7d04a9e21737e15a4e43799
AndreaRinaldi1 <andrea5.rinaldi@mail.polimi.it>
2017-07-04 00:08:12 +0200

    javadoc core package

--- src/main/java/it/polimi/ingsw/GC_28/core/TakeCardAction.java
+++ src/main/java/it/polimi/ingsw/GC_28/core/TakeCardAction.java
@@ -14,7 +14,11 @@ import it.polimi.ingsw.GC_28.effects.TakeCardEffect;
 import it.polimi.ingsw.GC_28.model.GameModel;
 import it.polimi.ingsw.GC_28.server.Message;
 import it.polimi.ingsw.GC_28.view.GameView;
-
+/**
+ * This class allows the player to take a card on the gameBoard
+ * @author andreaRinadli
+ * @version 1.0, 07/03/2017
+ */
 public class TakeCardAction extends Action{
 	private GameView game;
 	private GameBoard gameBoard;
@@ -24,6 +28,12 @@ public class TakeCardAction extends Action{
 	private TakeCardEffect throughEffect;
 	private GameModel gameModel;
 	
+	/**
+	 * This constructor builds a TakeCardAction out of the gameView, necessary to apply the effects, 
+	 * and the gameModel, useful for controlling if the action is possible and apply the changes
+	 * @param game the view of the game
+	 * @param gameModel the model of the game
+	 */
 	public TakeCardAction(GameView game, GameModel gameModel){
 		this.game = game;
 		this.gameBoard = gameModel.getGameBoard();
@@ -31,24 +41,43 @@ public class TakeCardAction extends Action{
 		takeCardController = new TakeCardController(gameModel);
 	}
 
+	/**
+	 * This method set for the action, the family member that the player has chosen to use
+	 * @param familyMember the chosen family member for this action
+	 */
 	public void setFamilyMember(FamilyMember familyMember) {
 		this.familyMember = familyMember;
 	}
 
-
+	/**
+	 * This method set the name of the card the player has chosen to take
+	 * @param name the name of the card
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
+	/**
+	 * This method defines if this action is carried out through the immediate effect of a just taken card, 
+	 * and not by a common action of the player
+	 * @param throughEffect the effect and its properties through which the player is exploiting this action
+	 */
 	public void setThroughEffect(TakeCardEffect throughEffect) {
 		this.throughEffect = throughEffect;
 	}
+	
+	/**
+	 * @return if the action chosen by the player is applicable or not
+	 */
 	@Override
 	public boolean isApplicable(){
 		return takeCardController.check(game, name, familyMember, throughEffect);
 	}
 	
+	/**
+	 * this method carries out the actual final application of the action exploited by the player
+	 * and notifies the view with the result
+	 */
 	@Override
 	public void apply(){
 		takeCardController.reduce3Coins(familyMember, false, null);
